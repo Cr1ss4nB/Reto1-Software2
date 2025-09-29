be54d70bb7f58f6096116d782ec2bf51822e9d03
Laura Figueredo <laura.figueredo02@uptc.edu.co>
2025-09-29 18:44:39 -0500

    feat: changes in controller

--- loginMicroservice/src/main/java/co/edu/uptc/loginmicroservice/controller/UserController.java
+++ loginMicroservice/src/main/java/co/edu/uptc/loginmicroservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package co.edu.uptc.loginmicroservice.controller;
 
+import co.edu.uptc.loginmicroservice.config.JwtUtil;
 import co.edu.uptc.loginmicroservice.domain.User;
 import co.edu.uptc.loginmicroservice.dto.LoginRequest;
 import co.edu.uptc.loginmicroservice.service.UserService;
@@ -8,13 +9,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/login")
 public class UserController {
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/createuser")
@@ -24,7 +30,17 @@ public class UserController {
     }
 
     @PostMapping("/authuser")
-    public boolean authUser(@RequestBody LoginRequest request) {
-        return userService.authenticateUser(request.getCustomerId(), request.getPassword());
+    public Map<String,Object> authUser(@RequestBody LoginRequest request) {
+        boolean valid= userService.authenticateUser(request.getCustomerId(), request.getPassword());
+        Map<String,Object> response = new HashMap<>();
+        if (valid) {
+           String token = jwtUtil.generateToken(request.getCustomerId());
+           response.put("autenticated", true);
+           response.put("token", token);
+        }else {
+            response.put("autenticated", false);
+            response.put("message", "Credenciales inválidas");
+        }
+        return response;
     }
 }
--- loginMicroservice/src/main/java/co/edu/uptc/loginmicroservice/service/UserService.java
+++ loginMicroservice/src/main/java/co/edu/uptc/loginmicroservice/service/UserService.java
@@ -2,21 +2,27 @@ package co.edu.uptc.loginmicroservice.service;
 
 import co.edu.uptc.loginmicroservice.domain.User;
 import co.edu.uptc.loginmicroservice.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public void createUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
     public boolean authenticateUser(String customerId, String password) {
-        return userRepository.findByCustomerAndPassword(customerId, password).isPresent();
+        Optional<User> userOpt = userRepository.findById(customerId);
+        return userOpt.isPresent() && encoder.matches(password, userOpt.get().getPassword());
     }
 }
